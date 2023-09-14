dc81559d174177699ec4bd432a4f2eafd93fb34a
andersonlouzada <anderson.s.louzada@gmail.com>
2023-09-14 10:50:55 -0300

    tira alguns comentários do teste

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -12,57 +12,43 @@ describe('POST /login', function () {
   beforeEach(function () { sinon.restore(); });
 
   it('ao não receber um e-mail, retorne um erro', async function () {
-    // Arrange
     const httpRequestBody = loginMock.noUsername;
 
-    // Act
     const httpResponse = await chai.request(app).post('/login').send(httpRequestBody);
 
-    // Assert
     expect(httpResponse.status).to.equal(400);
     expect(httpResponse.body).to.be.deep.equal({ message: '"username" and "password" are required' });
 
   })
 
   it('ao não receber uma senha, retorne um erro', async function () {
-    // Arrange
     const httpRequestBody = loginMock.noPassword
 
-    // Act
     const httpResponse = await chai.request(app).post('/login').send(httpRequestBody);
 
-    // Assert
     expect(httpResponse.status).to.equal(400);
     expect(httpResponse.body).to.be.deep.equal({ message: '"username" and "password" are required' });
   });
 
   it('ao receber um e-mail inexistente, retorne um erro', async function () {
-    // Arrange
     const httpRequestBody = loginMock.notExistingUserBody
     sinon.stub(UserModel, 'findOne').resolves(null);
 
-    // Act
     const httpResponse = await chai.request(app).post('/login').send(httpRequestBody);
 
-    // Assert
     expect(httpResponse.status).to.equal(401);
     expect(httpResponse.body).to.be.deep.equal({ message: 'Username or password invalid' });
   });
 
   it('ao receber um e-mail existente e uma senha errada, retorne um erro', async function () {
-    // Arrange
     const httpRequestBody = loginMock.existingUserWithWrongPasswordBody 
-    /* Esta linha será responsável por definir um valor derivado de loginMock.existingUser
-    que é compatível com o tipo esperado como retorno da função `findOne()`  */
+
     const mockFindOneReturn = UserModel.build(loginMock.existingUser);
-    /* Aqui utilizamos o valor de mockFindOneReturn pois ele é compatível com o retorno da função `findOne()` */
     sinon.stub(UserModel, 'findOne').resolves(mockFindOneReturn);
 
-    // Act
     const httpResponse = await chai.request(app).post('/login')
       .send(httpRequestBody);
 
-    // Assert
     expect(httpResponse.status).to.equal(401);
     expect(httpResponse.body).to.be.deep.equal({ message: 'Username or password invalid' });
   });
